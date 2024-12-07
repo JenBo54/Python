ce4c55b48e1418f8633b56dc801bf967eb24b0cd
JenBo54 <jenbo2627@gmail.com>
2024-12-07 21:27:00 +0700

    first commit

---
+++ leeson_2_type.py
@@ -0,0 +1,123 @@
+# print(type(3.5))
+
+
+# num = 2
+# num_type = type(num)
+# print(type(num_type))
+
+
+
+# num_2=4
+# print=(type(num_2))
+# num_str_2=str(num_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num_inp_1=input("==)")
+# print("num_inp_1=", num_inp_1)
+# print(type(num_inp_1))
+
+
+
+
+
+
+
+num_inp_1=input("==)")
+print("ответ:", int(num_inp_1*12))
+print(type(num_inp_1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
